1a2daad36efa1aacf02e93a7831e7e6613948510
Paller1337 <max.paller@yandex.ru>
2024-01-16 16:46:17 +0300

    .

--- helpers/axiosInstance.ts
+++ helpers/axiosInstance.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 // Создание экземпляра axios с базовым URL для стороны сервера
 export const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://portal.kplazma.ru' : 'http://192.168.1.19:5000'
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://portal.kplazma.ru' : 'https://portal.kplazma.ru'
 });
